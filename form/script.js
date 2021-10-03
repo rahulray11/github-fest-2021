32cdb520501d5324d168992e05d8bba93e09117e
sjostr <forty.three.kjs@gmail.com>
2021-10-03 09:44:24 -0500

    Adds code formatting

--- form/script.js
+++ form/script.js
@@ -1,9 +1,10 @@
-const name = document.forms["myForm"]['name'] .value;
-        const cls = document.getElementById("cls").value;
-        function validate(){
-        if(name==''){
-            alert('please filled name field.');
-            return false;
-        }
-        
-    }
+const name = document.forms["myForm"]['name'].value;
+const cls = document.getElementById("cls").value;
+
+function validate() {
+	if (name == '') {
+		alert('please filled name field.');
+		return false;
+	}
+
+}
