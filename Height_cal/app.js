5c322b5e69ccdca789d3c4fde682513f8f042aab
snowsmasher <91797699+snowsmasher@users.noreply.github.com>
2021-10-05 16:22:40 +1100

    Arrow Functions and Style Improvements

--- Height_cal/app.js
+++ Height_cal/app.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('form');
 const dark = document.querySelector('#dark-mode');
 
-form.addEventListener('submit',function(e){
+form.addEventListener('submit', (e) => {
     let feet = document.querySelector('#feet');
     let inches = document.querySelector('#inches');
     const results = document.querySelector('#results');
@@ -9,23 +9,20 @@ form.addEventListener('submit',function(e){
     feet = parseInt(feet.value);
     inches = parseInt(inches.value);
 
-    if(isNaN(feet) || isNaN(inches)){
+    if (isNaN(feet) || isNaN(inches)){
         results.textContent = "Please Enter a valid number!";
-    }
-    else if(feet < 0){
+    } else if (feet < 0) {
         results.textContent = "please Enter a feet value > 0";
-    }
-    else if(inches <0 || inches >=12){
+    } else if (inches < 0 || inches >= 12) {
         results.textContent = "please Enter a inches value  between 0 and 12";
-    }
-    else{
-        let totalInches = parseInt((feet*30.48) + (inches*2.54));
-        
+    } else {
+        let totalInches = parseInt((feet * 30.48) + (inches * 2.54));
         results.textContent = `${totalInches} cm`;
         document.querySelector('#feet').value = '';
         document.querySelector('#inches').value = '';
     }
 })
-function dark(){
-	document.querySelector('body').style.colorb='red';
+
+const dark = () => {
+    document.querySelector('body').style.colorb='red';
 }
