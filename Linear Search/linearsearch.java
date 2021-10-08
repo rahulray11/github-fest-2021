3b0747b9be41dfda0f677411dd7d85642d7caf23
abdelbasset4 <benatmani1999@gmail.com>
2021-10-08 21:59:48 +0100

    Create JS background change every 2 second

--- Linear Search/linearsearch.java
+++ Linear Search/linearsearch.java
@@ -1,4 +1,5 @@
-public class LinearSearchExample{    
+// Java program to implement Linear Search
+public class LinearSearch{    
 public static int linearSearch(int[] arr, int key){    
         for(int i=0;i<arr.length;i++){    
             if(arr[i] == key){    
@@ -9,7 +10,7 @@ public static int linearSearch(int[] arr, int key){
     }    
     public static void main(String a[]){    
         int[] a1= {10,20,30,50,70,90};    
-        int key = 50;    
+        int key = 30;    
         System.out.println(key+" is found at index: "+linearSearch(a1, key));    
     }    
 }    
