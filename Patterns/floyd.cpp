6e6f62ef4fa9ffaee2f4b1cfe801d12947bb031f
Ayaz <aayyaazz123@gmail.com>
2021-10-12 13:22:04 +0530

    Floyd Pattern

---
+++ Patterns/floyd.cpp
@@ -0,0 +1,23 @@
+// Floyd's Triangle
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    int count = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout<<count<<" ";
+            count++;
+        }
+        
+        cout<<endl;
+    }
+    
+    return 0;
+}
