a16f04ce945432821799e3c042fd7c2cf1669554
Muhammad A Faishal <maafaishal@gmail.com>
2021-10-18 01:26:57 +0700

    style: update©

--- LengthConverter/script.js
+++ LengthConverter/script.js
@@ -1,5 +1,6 @@
 function lengthConverter(source, valNum) {
   valNum = parseFloat(valNum);
+
   var inputFeet = document.getElementById("inputFeet");
   var inputMeters = document.getElementById("inputMeters");
   var inputInches = document.getElementById("inputInches");
@@ -7,22 +8,7 @@ function lengthConverter(source, valNum) {
   var inputYards = document.getElementById("inputYards");
   var inputKilometers = document.getElementById("inputKilometers");
   var inputMiles = document.getElementById("inputMiles");
-  if (source == "inputFeet") {
-    inputMeters.value = (valNum / 3.2808).toFixed(2);
-    inputInches.value = (valNum * 12).toFixed(2);
-    inputcm.value = (valNum / 0.032808).toFixed();
-    inputYards.value = (valNum * 0.33333).toFixed(2);
-    inputKilometers.value = (valNum / 3280.8).toFixed(5);
-    inputMiles.value = (valNum * 0.00018939).toFixed(5);
-  }
-  if (source == "inputMeters") {
-    inputFeet.value = (valNum * 3.2808).toFixed(2);
-    inputInches.value = (valNum * 39.37).toFixed(2);
-    inputcm.value = (valNum / 0.01).toFixed();
-    inputYards.value = (valNum * 1.0936).toFixed(2);
-    inputKilometers.value = (valNum / 1000).toFixed(5);
-    inputMiles.value = (valNum * 0.00062137).toFixed(5);
-  }
+
   if (source == "inputInches") {
     inputFeet.value = (valNum * 0.083333).toFixed(3);
     inputMeters.value = (valNum / 39.37).toFixed(3);
@@ -62,5 +48,21 @@ function lengthConverter(source, valNum) {
     inputcm.value = (valNum / 0.0000062137).toFixed();
     inputYards.value = (valNum * 1760).toFixed();
     inputKilometers.value = (valNum / 0.62137).toFixed(2);
+  }
+  if (source == "inputFeet") {
+    inputMeters.value = (valNum / 3.2808).toFixed(2);
+    inputInches.value = (valNum * 12).toFixed(2);
+    inputcm.value = (valNum / 0.032808).toFixed();
+    inputYards.value = (valNum * 0.33333).toFixed(2);
+    inputKilometers.value = (valNum / 3280.8).toFixed(5);
+    inputMiles.value = (valNum * 0.00018939).toFixed(5);
+  }
+  if (source == "inputMeters") {
+    inputFeet.value = (valNum * 3.2808).toFixed(2);
+    inputInches.value = (valNum * 39.37).toFixed(2);
+    inputcm.value = (valNum / 0.01).toFixed();
+    inputYards.value = (valNum * 1.0936).toFixed(2);
+    inputKilometers.value = (valNum / 1000).toFixed(5);
+    inputMiles.value = (valNum * 0.00062137).toFixed(5);
   }
 }
